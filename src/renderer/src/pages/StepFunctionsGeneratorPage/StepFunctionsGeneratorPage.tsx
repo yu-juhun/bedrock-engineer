4168874b3c925c06bae3d03950f53c80eca2579a
Daisuke Awaji <gee.awa@gmail.com>
2025-03-06 01:55:54 +0900

    fix #23

--- src/renderer/src/pages/StepFunctionsGeneratorPage/StepFunctionsGeneratorPage.tsx
+++ src/renderer/src/pages/StepFunctionsGeneratorPage/StepFunctionsGeneratorPage.tsx
@@ -140,8 +140,8 @@ function StepFunctionsGeneratorPage() {
       {/* Buttom Input Field Block */}
       <div className="flex gap-2 fixed bottom-0 left-20 right-5 bottom-3">
         <div className="relative w-full">
-          <div className="flex gap-2 justify-between">
-            <div>
+          <div className="flex gap-2 justify-between mb-4">
+            <div className="flex flex-wrap gap-2">
               {examples.map((e, index) => {
                 return (
                   <motion.button
@@ -149,7 +149,21 @@ function StepFunctionsGeneratorPage() {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ delay: index * 0.2 }}
                     key={e.title}
-                    className="cursor-pointer rounded-full border p-2 text-xs hover:border-gray-300 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-gray-700 "
+                    className="
+                      cursor-pointer
+                      rounded-full
+                      border
+                      p-2
+                      text-xs
+                      text-gray-700
+                      dark:text-gray-200
+                      hover:border-gray-400
+                      hover:bg-gray-100
+                      dark:hover:bg-gray-700
+                      dark:hover:border-gray-500
+                      transition-colors
+                      duration-200
+                    "
                     onClick={() => {
                       setUserInput(e.value)
                     }}
@@ -171,24 +185,6 @@ function StepFunctionsGeneratorPage() {
             setIsComposing={setIsComposing}
             sendMsgKey={sendMsgKey}
           />
-          {/* <textarea
-            onCompositionStart={() => setIsComposing(true)}
-            onCompositionEnd={() => setIsComposing(false)}
-            className={`block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 mt-2 dark:text-white dark:bg-gray-800`}
-            placeholder={t('What kind of step functions will you create?')}
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-            onKeyDown={onkeydown}
-            required
-            disabled={loading}
-            rows={3}
-          />
-          <button
-            onClick={() => handleSubmit(userInput, messages)}
-            className="absolute end-2.5 bottom-2.5 rounded-lg hover:bg-gray-200 px-2 py-2 dark:hover:bg-gray-700 dark:text-white"
-          >
-            <FiSend className="text-xl" />
-          </button> */}
         </div>
       </div>
     </div>
